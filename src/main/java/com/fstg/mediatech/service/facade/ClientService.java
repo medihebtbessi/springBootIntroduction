6bfbcf82dda956666765284ae50ee64e9ec69ce9
medihebtbessi <mohamediheb.tbessi@esprit.tn>
2024-03-22 14:31:38 +0100

    final project

--- src/main/java/com/fstg/mediatech/service/facade/ClientService.java
+++ src/main/java/com/fstg/mediatech/service/facade/ClientService.java
@@ -4,13 +4,17 @@ import com.fstg.mediatech.dto.ClientRequestDto;
 import com.fstg.mediatech.dto.ClientResponseDto;
 import com.fstg.mediatech.models.ClientEntity;
 
+import java.util.List;
+
 public interface ClientService {
 
    ClientResponseDto save(ClientRequestDto clientRequestDto);
    ClientResponseDto findById(Integer id);
    ClientResponseDto findByNom(String nom);
    void delete(Integer id);
-   ClientResponseDto update(ClientRequestDto clientRequestDto,Integer id);
+   ClientResponseDto update(ClientRequestDto clientRequestDto,Integer id) throws Exception;
+
+   List<ClientResponseDto> findAll();
 
 
 }
